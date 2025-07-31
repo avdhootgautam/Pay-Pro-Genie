3316c5a874f26ba578bb141d26762255a6a1c339
avdhootgautam <avdhootgautam416@gmail.com>
2025-07-31 23:31:06 +0530

    Continued with FileCard

--- frontend/src/components/auth/ResultPageBody.jsx
+++ frontend/src/components/auth/ResultPageBody.jsx
@@ -1,6 +1,12 @@
 import {Box} from "@mui/material"
 import styles from "../../styles/ResultPageBody.module.css"
+import {useState} from "react"
 const ResultPageBody=()=>{
+    const [fileName,setFilename]=useState("")
+    const [rowCount,setrowCount]=useState(0)
+    const [columnCount,setColumnCount]=useState(0)
+    const [fileSize,setFileSize]=useState("")
+    const [fileType,setFileType]=useState("")
     return(
     <Box className={styles.BodyContainer}>
         
