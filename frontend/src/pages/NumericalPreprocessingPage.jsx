e441baeb491fbcbf6e1cda5e2e4ed00d8fd48acd
avdhootgautam <avdhootgautam416@gmail.com>
2025-09-16 21:24:05 +0530

    Created a Numerical preprocessing page

---
+++ frontend/src/pages/NumericalPreprocessingPage.jsx
@@ -0,0 +1,11 @@
+import {Box} from "@mui/material"
+import Header from "../components/auth/Header"
+import styles from "../styles/NumericalPreprocessingPage.module.css"
+const NumericalPreprocessingPage=()=>{
+    return (
+        <Box className={styles.Container}>
+            <Header/>
+        </Box>
+    )
+}
+export default NumericalPreprocessingPage;
